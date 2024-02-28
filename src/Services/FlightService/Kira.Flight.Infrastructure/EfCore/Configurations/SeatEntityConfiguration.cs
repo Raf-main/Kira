445cc822a9f9962750b59a0b59ac23d19397452a
Raf <rafikrrrafik@gmail.com>
2024-02-28 08:55:34 +0300

    add init migration

---
+++ src/Services/FlightService/Kira.Flight.Infrastructure/EfCore/Configurations/SeatEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Kira.Flight.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kira.Flight.Infrastructure.EfCore.Configurations
+{
+    public class SeatEntityConfiguration : IEntityTypeConfiguration<Seat>
+    {
+        public void Configure(EntityTypeBuilder<Seat> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedNever();
+            builder.Property(p => p.IsReserved).IsRequired();
+            builder.Property(p => p.SeatNumber).IsRequired().HasMaxLength(32);
+            builder.Ignore(p => p.DomainEvents);
+        }
+    }
+}
---
+++ src/Services/FlightService/Kira.Flight.Infrastructure/Migrations/20240224034008_initial.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kira.Flight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airplanes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    Model = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airplanes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SeatNumber = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    FlightId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsReserved = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AirplaneId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FromAirportId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ToAirportId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    UtcDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airplanes_AirplaneId",
+                        column: x => x.AirplaneId,
+                        principalTable: "Airplanes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_FromAirportId",
+                        column: x => x.FromAirportId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_ToAirportId",
+                        column: x => x.ToAirportId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_AirplaneId",
+                table: "Flights",
+                column: "AirplaneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_FromAirportId",
+                table: "Flights",
+                column: "FromAirportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_ToAirportId",
+                table: "Flights",
+                column: "ToAirportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Seats");
+
+            migrationBuilder.DropTable(
+                name: "Airplanes");
+
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
