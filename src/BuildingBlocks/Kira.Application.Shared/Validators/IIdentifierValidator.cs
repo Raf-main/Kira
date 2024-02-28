6be00d086221a07a5a72125f811a657e900d647f
Raf <rafikrrrafik@gmail.com>
2024-02-28 08:54:35 +0300

    add identifier validator

---
+++ src/BuildingBlocks/Kira.Application.Shared/Validators/IIdentifierValidator.cs
@@ -0,0 +1,6 @@
+﻿using FluentValidation;
+
+namespace Kira.Application.Shared.Validators
+{
+    public interface IIdentifierValidator<in T> : IValidator<T>;
+}
---
+++ src/BuildingBlocks/Kira.Application.Shared/Validators/IdentifierValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Kira.Application.Shared.Validators
+{
+    public class IdentifierValidator<T> : AbstractValidator<T>, IIdentifierValidator<T>
+    {
+        public IdentifierValidator()
+        {
+            RuleFor(x => x).NotEmpty().WithMessage("Id can't be empty");
+        }
+    }
+}
