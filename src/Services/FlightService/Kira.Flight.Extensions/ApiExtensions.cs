7e5def828b826205d2182e12aaa933dcf6488e59
Raf <rafikrrrafik@gmail.com>
2024-02-28 08:55:16 +0300

    add flight api configuration extensions

---
+++ src/Services/FlightService/Kira.Flight.Extensions/ApiExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Kira.Flight.Extensions
+{
+    public static class ApiExtensions
+    {
+        public static void AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "Flight API" });
+            });
+        }
+    }
+}
---
+++ src/Services/FlightService/Kira.Flight.Extensions/ApplicationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Kira.Application.Shared.Validators;
+using Kira.Flight.Application.Behaviors;
+using Kira.Flight.Application.Features.Airplanes.Commands.CreateAirplane;
+using Kira.Flight.Application.Mappers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kira.Flight.Extensions
+{
+    public static class ApplicationExtensions
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(mediatrConfiguration =>
+            {
+                mediatrConfiguration.RegisterServicesFromAssembly(Assembly.GetAssembly(typeof(CreateAirplaneCommand)) ?? throw new Exception());
+                mediatrConfiguration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                mediatrConfiguration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssemblyContaining(typeof(CreateAirplaneCommandValidator));
+            services.AddScoped<IValidator<Guid>, IdentifierValidator<Guid>>();
+            services.AddScoped<IIdentifierValidator<Guid>, IdentifierValidator<Guid>>();
+            services.AddAutoMapper(Assembly.GetAssembly(typeof(AirplaneMapper)));
+        }
+    }
+}
