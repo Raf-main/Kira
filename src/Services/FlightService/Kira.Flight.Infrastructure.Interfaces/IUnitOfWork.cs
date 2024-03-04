4d2ab4a26115fd1c5a8d2f9ef984b1ffcc0f494d
Raf <rafikrrrafik@gmail.com>
2024-03-04 16:51:28 +0300

    add unitofwork

---
+++ src/Services/FlightService/Kira.Flight.Infrastructure.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Kira.Flight.Domain.Entities;
+using Light.Infrastructure.Extensions.Repositories;
+
+namespace Kira.Flight.Infrastructure.Interfaces
+{
+    public interface  IUnitOfWork
+    {
+        IAsyncWriteRepository<Airplane, Guid> AirplaneWriteRepository { get; }
+        IAsyncWriteRepository<Airport, Guid> AirportWriteRepository { get; }
+        IAsyncWriteRepository<Domain.Entities.Flight, Guid> FlightWriteRepository { get; }
+        IAsyncWriteRepository<Seat, Guid> SeatWriteRepository { get; }
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Services/FlightService/Kira.Flight.Infrastructure/EfCore/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Kira.Flight.Domain.Entities;
+using Kira.Flight.Infrastructure.Interfaces;
+using Light.Infrastructure.EfCore.Repositories;
+using Light.Infrastructure.Extensions.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kira.Flight.Infrastructure.EfCore.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected readonly DbContext Context;
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+            AirplaneWriteRepository = new WriteEfRepository<Airplane, Guid>(Context);
+            AirportWriteRepository = new WriteEfRepository<Airport, Guid>(Context);
+            FlightWriteRepository = new WriteEfRepository<Domain.Entities.Flight, Guid>(Context);
+            SeatWriteRepository = new WriteEfRepository<Seat, Guid>(Context);
+        }
+
+        public IAsyncWriteRepository<Airplane, Guid> AirplaneWriteRepository { get; }
+        public IAsyncWriteRepository<Airport, Guid> AirportWriteRepository { get; }
+        public IAsyncWriteRepository<Domain.Entities.Flight, Guid> FlightWriteRepository { get; }
+        public IAsyncWriteRepository<Seat, Guid> SeatWriteRepository { get; }
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            await Context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
