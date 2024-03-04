16fe180958a812b593a8cadc995411bbf895eccb
Raf <rafikrrrafik@gmail.com>
2024-03-04 16:44:46 +0300

    add exception handling middleware

---
+++ src/Services/FlightService/Kira.Flight.API/Middleware/ExceptionHandlerExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Kira.Flight.API.Middleware
+{
+    public static class ExceptionHandlerExtensions
+    {
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
---
+++ src/Services/FlightService/Kira.Flight.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Text.Json;
+using FluentValidation;
+using Kira.Flight.Application.Exceptions;
+
+namespace Kira.Flight.API.Middleware
+{
+    public class ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                response.StatusCode = exception switch
+                {
+                    ValidationException => (int)HttpStatusCode.BadRequest,
+                    NotFoundException => (int)HttpStatusCode.NotFound,
+                    _ => (int)HttpStatusCode.InternalServerError
+                };
+
+                logger.LogError(exception, "Middleware caught error");
+
+                string result;
+
+                if (exception is ValidationException validationException)
+                {
+                    var errors = new Dictionary<string, List<string>>();
+
+                    foreach (var failure in validationException.Errors)
+                    {
+                        var field = failure.PropertyName;
+                        var errorMessage = failure.ErrorMessage;
+
+                        if (!errors.TryGetValue(field, out var value))
+                        {
+                            value = new List<string>();
+                            errors[field] = value;
+                        }
+
+                        value.Add(errorMessage);
+                    }
+
+                    result = JsonSerializer.Serialize(new { Errors = errors });
+                }
+                else
+                {
+                    result = JsonSerializer.Serialize(new { message = exception.Message });
+                }
+
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
